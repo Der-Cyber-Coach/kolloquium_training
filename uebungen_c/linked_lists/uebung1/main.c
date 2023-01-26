fc86e3318077c018e0f0344f0be06d479f7b1e97
Simon Löschke <simon-loeschke@posteo.de>
2023-01-26 20:47:24 +0100

    aufwärmübung

--- uebungen_c/linked_lists/uebung1/main.c
+++ uebungen_c/linked_lists/uebung1/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
+int test(int a);
 
 int main(void)
 
 {
+    for(int i =0; i <= 199;i++)
+    {
+    printf("%d\n",test(i));
+    };
     return 0;
+}
+
+
+
+int test(int a) 
+{
+    int b = 12;
+
+    return a+b;
+
 }
